fa80d65215bb4181193dc3286ebef5729e4aaffd
Anthony Flores <anthony.florescarrasco@gmail.com>
2020-07-07 21:42:27 -0500

    Initial configuration for the project

---
+++ src/main/java/com/demo/avla/AvlaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.demo.avla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvlaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvlaBackendApplication.class, args);
+	}
+
+}
